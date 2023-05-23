62d1a001b1c3822981d084c5c86431eaecaa29ed
MadhushaPrasad <madushaprasad21@gmail.com>
2023-05-23 20:21:12 +0530

    display supermarket price details

--- src/components/SupermarketPrice/SupermarketPriceTable/SupermarketPriceTable.jsx
+++ src/components/SupermarketPrice/SupermarketPriceTable/SupermarketPriceTable.jsx
@@ -3,7 +3,7 @@ import {useDispatch, useSelector} from 'react-redux'
 import {getAllSupermarketPriceDetails} from "../../../store/reducers/supermarketPriceReducer.js";
 
 function SupermarketPriceTable(props) {
-
+    const supermarketPriceState = useSelector(state => state.supermarketPrice);
     const supermarketPriceDetails = useSelector(state => state.supermarketPrice.supermarketPrices);
     const dispatch = useDispatch();
 
@@ -14,29 +14,54 @@ function SupermarketPriceTable(props) {
 
     console.log(supermarketPriceDetails);
 
-    return (
-        <div className="item">
-            <div className="row">
-                <h6 className="mb-0 fw-bold mt-2 mb-2">Supermarkets prices list in Sri Lanka</h6>
-                <div className="table-responsive">
-                    <table className="table table-striped custom-table" id="assignLabsTable">
-                        <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Item Image</th>
-                            <th scope="col">Item Name</th>
-                            <th scope="col">Yesterday Price</th>
-                            <th scope="col">Today Price</th>
-                            <th scope="col"></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
+    return (<div className="item">
+        <div className="row">
+            <h6 className="mb-0 fw-bold mt-2 mb-2">Supermarkets prices list in Sri Lanka</h6>
+            <div className="table-responsive">
+                <table className="table table-striped custom-table" id="assignLabsTable">
+                    <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Item Image</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Yesterday Price</th>
+                        <th scope="col">Today Price</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {supermarketPriceState.loading && <span>Loading...</span>}
+                    {!supermarketPriceState.loading && supermarketPriceState.error ?
+                        <div>Error: {supermarketPriceState.error}</div> : null}
+                    {!supermarketPriceState.loading && supermarketPriceDetails.length ?
+                        (
+                            supermarketPriceState.supermarketPrices.map(superMarket =>
+                                (
+                                    <tr itemScope="row" id={superMarket._id} key={superMarket._id}>
+                                        <td>
+                                            {superMarket.superMarketPriceID}
+                                        </td>
+                                        <td>{superMarket.itemName}</td>
+                                        <td>{superMarket.itemName}</td>
+                                        <td>
+                                            {superMarket.yesterDayPrice}
+                                        </td>
+                                        <td>
+                                            {superMarket.toDayPrice}
+                                        </td>
+                                        <td>
+                                            <i className="fa-solid fa-pen me-3 text-primary"></i>
+                                            <i className="fa-solid fa-trash-can d-inline me-2 text-danger"></i>
+                                        </td>
+                                    </tr>
+                                )
+                            )
+                        ) : null}
+                    </tbody>
+                </table>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default SupermarketPriceTable;
