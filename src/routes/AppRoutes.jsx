757fc74ae79eee9eaf37f37ddf5282f402decd2b
MadhushaPrasad <madushaprasad21@gmail.com>
2023-05-24 12:41:49 +0530

    rename SuperMarket Price folder

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SuperMarketPricePage from "../pages/supermarket/SuperMarketPricePage.jsx";
+import SuperMarketPricePage from "../pages/Supermarket/SuperMarketPricePage.jsx";
 import Header from "../components/common/header/Header";
 import StoreManagementPage from "../pages/StoreManagement/StoreManagementPage.jsx";
 
