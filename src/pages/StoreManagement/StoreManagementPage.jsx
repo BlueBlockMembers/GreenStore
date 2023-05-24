3b96b064196d57c3b8c27f39ea1248e075c7306c
Ishan <88660439+Nabouru@users.noreply.github.com>
2023-05-24 12:21:30 +0530

    wrapped with admin layout

--- src/pages/StoreManagement/StoreManagementPage.jsx
+++ src/pages/StoreManagement/StoreManagementPage.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import ListComponent from "../../components/StoreManagement/ToolAndSeedList/ListComponent";
 import CountContainer from "../../components/StoreManagement/SeedCount/CountContainer";
+import AdminLayout from "../../layouts/adminLayout";
 
 const StoreManagementPage = () => {
   return (
-    <div className="" style={{ marginTop: "100px" }}>
-      <div style={{ marginLeft: "220px", marginRight: "250px" }}>
-        <CountContainer />
-      </div>
+    <AdminLayout class="wrapper">
+      <div className="" style={{ marginTop: "100px" }}>
+        <div style={{ marginLeft: "220px", marginRight: "250px" }}>
+          <CountContainer />
+        </div>
 
-      <div className="row " style={{ marginLeft: "150px" }}>
-        <div className="col 1 ">
-          <ListComponent title={"Tool List"} />
-        </div>
-        <div className="col 1">
-          <ListComponent title={"Seed List"} />
+        <div className="row " style={{ marginLeft: "150px" }}>
+          <div className="col 1 ">
+            <ListComponent title={"Tool List"} />
+          </div>
+          <div className="col 1">
+            <ListComponent title={"Seed List"} />
+          </div>
         </div>
       </div>
-    </div>
+    </AdminLayout>
   );
 };
 
