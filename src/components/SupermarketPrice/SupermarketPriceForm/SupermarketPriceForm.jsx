f2beca8159719665b5f01ea753e53384000c1c69
MadhushaPrasad <madushaprasad21@gmail.com>
2023-05-26 18:00:50 +0530

    display validation error

--- src/components/SupermarketPrice/SupermarketPriceForm/SupermarketPriceForm.jsx
+++ src/components/SupermarketPrice/SupermarketPriceForm/SupermarketPriceForm.jsx
@@ -1,19 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {add, getAll, update} from "../../../store/actions/supermarketPriceActions.js";
+import {add, getAll, update} from "../../../store/actions/supermarketPriceActions";
 import Swal from 'sweetalert2'
 import validateSupermarketPrice from "../../../validations/supermarketPrice.validation.js";
+import {getAllProducts} from "../../../store/actions/productsActions.js";
 
 
 function SupermarketPriceForm() {
     const supermarketPriceState = useSelector(state => state.supermarketPrice);
+    const productState = useSelector(state => state.products);
     const dispatch = useDispatch();
     const [id, setId] = useState("")
     const [superMarketPriceID, setSuperMarketPriceID] = useState("")
-    const [ItemId, setItemId] = useState("645b423fc9af1470fb4712fe")
+    const [ItemId, setItemId] = useState("")
     const [itemName, setItemName] = useState("")
+    const [searchItemName, setSearchItemName] = useState("")
     const [yesterdayPrice, setYesterdayPrice] = useState("")
     const [todayPrice, setTodayPrice] = useState("")
+    const [productList, setProductList] = useState([]);
+    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         if (supermarketPriceState.selectedSupermarketPrice._id) {
@@ -31,6 +36,7 @@ function SupermarketPriceForm() {
             setYesterdayPrice(supermarketPriceState.yesterDayPrice)
             setTodayPrice(supermarketPriceState.toDayPrice)
         }
+        loadAlProducts();
     }, [supermarketPriceState.selectedSupermarketPrice])
 
     const addSupermarketPrice = async () => {
@@ -45,6 +51,7 @@ function SupermarketPriceForm() {
         const {errors, isInvalid} = validateSupermarketPrice(payload);
 
         if (isInvalid) {
+            setErrors(errors);
             Swal.fire({
                 toast: true,
                 position: 'top-end',
@@ -54,6 +61,7 @@ function SupermarketPriceForm() {
                 title: 'Please enter your details',
             });
         } else {
+            setErrors({})
             await dispatch(add(payload)).then((res) => {
                 if (!supermarketPriceState.error) {
                     Swal.fire({
@@ -93,7 +101,6 @@ function SupermarketPriceForm() {
         dispatch(getAll());
     };
 
-
     const handleUpdateSubmit = async () => {
         const payload = {
             _id: id,
@@ -104,43 +111,60 @@ function SupermarketPriceForm() {
             toDayPrice: todayPrice,
         };
 
-        if (supermarketPriceState.selectedSupermarketPrice.superMarketPriceID) {
-            await dispatch(update(payload)).then((res) => {
-                if (!supermarketPriceState.error) {
-                    Swal.fire({
-                        title: 'Success!',
-                        text: `${res.payload.message}`,
-                        icon: 'success',
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 3000,
-                    });
-                    clearForm();
-                } else {
+        const {errors, isInvalid} = validateSupermarketPrice(payload);
+
+        if (isInvalid) {
+            setErrors(errors);
+            Swal.fire({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                icon: 'error',
+                title: 'Please enter your details',
+            });
+        } else {
+            setErrors({})
+            if (supermarketPriceState.selectedSupermarketPrice.superMarketPriceID) {
+                await dispatch(update(payload)).then((res) => {
+                    if (!supermarketPriceState.error) {
+                        Swal.fire({
+                            title: 'Success!',
+                            text: `${res.payload.message}`,
+                            icon: 'success',
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                        });
+                        clearForm();
+                    } else {
+                        Swal.fire({
+                            title: 'Error!',
+                            text: `${supermarketPriceState.error}`,
+                            icon: 'error',
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                        });
+                    }
+                }).catch((err) => {
                     Swal.fire({
                         title: 'Error!',
-                        text: `${supermarketPriceState.error}`,
+                        text: `${err}`,
                         icon: 'error',
                         toast: true,
                         position: 'top-end',
                         showConfirmButton: false,
                         timer: 3000,
                     });
-                }
-            }).catch((err) => {
-                Swal.fire({
-                    title: 'Error!',
-                    text: `${err}`,
-                    icon: 'error',
-                    toast: true,
-                    position: 'top-end',
-                    showConfirmButton: false,
-                    timer: 3000,
                 });
-            });
-            dispatch(getAll());
+                dispatch(getAll());
+            }
         }
+
+        dispatch(getAll());
     };
 
     const clearForm = () => {
@@ -151,6 +175,21 @@ function SupermarketPriceForm() {
         setYesterdayPrice('')
         setTodayPrice('')
         document.getElementById("superMarketPriceForm").reset();
+    }
+
+    const loadAlProducts = () => {
+        dispatch(getAllProducts()).then((res) => {
+            setProductList(res.payload);
+        })
+    }
+
+    const getAllProductsNameAndId = () => {
+        return productList.map((item) => {
+            return (
+                <option key={item._id} value={item._id} className="form-control"
+                        id="itemNameDropDown">{item.name}</option>
+            )
+        });
     }
 
     return (<div className="item">
@@ -164,9 +203,9 @@ function SupermarketPriceForm() {
                             <div className="d-flex justify-content-end align-items-center">
                                 <div className="d-flex justify-content-center align-items-center">
                                     <input id="searchID" type="text" className="form-control col-8 me-5"
-                                           placeholder="Item Name" value={itemName}
+                                           placeholder="Item Name" value={searchItemName}
                                            onChange={(e) => {
-                                               setItemName(e.target.value)
+                                               setSearchItemName(e.target.value)
                                            }}
                                     />
                                 </div>
@@ -188,32 +227,41 @@ function SupermarketPriceForm() {
                                     value={ItemId}
                                     onChange={(e) => {
                                         setItemId(e.target.value)
+                                        setItemName(e.target.options[e.target.selectedIndex].text)
+                                        errors.itemName = ""
                                     }}>
-                                <option selected disabled value="645b423fc9af1470fb4712fe">Item</option>
-
+                                <option selected={true} disabled value="">Item</option>
+                                {getAllProductsNameAndId()}
                             </select>
                             <small
                                 htmlFor="itemName"
                                 className="d-block text-danger form-text invalid-feedback"
-                            ></small>
+                            >
+                                {errors.itemName}
+                            </small>
                         </div>
 
                         <div className="col-6 d-flex">
                                                    <span style={{"background": "#24353E"}}
-                                                         className="input-group-text d-inline text-white"
+                                                         className="input-group-text text-white"
                                                          id="basic-addon1">RS:</span>
                             <input id="yPrice" type="text"
-                                   className="form-control  d-inline"
+                                   className="form-control"
                                    placeholder="Yesterday Price" name="yprice"
                                    value={yesterdayPrice}
                                    onChange={(e) => {
                                        setYesterdayPrice(e.target.value)
+                                       errors.yesterDayPrice = ""
                                    }}/>
                         </div>
-                        <small
-                            htmlFor="yPrice"
-                            className="d-block text-danger form-text invalid-feedback"
-                        ></small>
+                        <div className="col-6 offset-6">
+                            <small
+                                htmlFor="yPrice"
+                                className="d-block text-danger form-text invalid-feedback"
+                            >
+                                {errors.yesterDayPrice}
+                            </small>
+                        </div>
                     </div>
 
                     <div className="row mt-4">
@@ -224,14 +272,18 @@ function SupermarketPriceForm() {
                             <input id="tPrice" type="text"
                                    className="form-control  d-inline"
                                    value={todayPrice}
+                                   required
                                    placeholder="Today Price" name="price" onChange={(e) => {
                                 setTodayPrice(e.target.value)
+                                errors.toDayPrice = ""
                             }}/>
                         </div>
                         <small
                             htmlFor="tPrice"
                             className="d-block text-danger form-text invalid-feedback"
-                        ></small>
+                        >
+                            {errors.toDayPrice}
+                        </small>
                     </div>
                     <div className="row mt-4">
                         <div className="d-flex justify-content-around align-items-center">
