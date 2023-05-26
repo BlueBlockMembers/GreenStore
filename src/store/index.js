37a1b9ded2e6123a03d6b3a340c057fce04a561f
MadhushaPrasad <madushaprasad21@gmail.com>
2023-05-26 18:01:54 +0530

    add product slice to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import SupermarketPriceReducer from './reducers/supermarketPriceReducer'
+import productsReducers from './reducers/productsReducers'
 
 
 export const store = configureStore({
     reducer: {
         supermarketPrice: SupermarketPriceReducer,
+        products: productsReducers
     }
 })
